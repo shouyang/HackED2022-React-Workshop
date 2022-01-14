69978ec78d8d7f487ad7021a84f51ba16e7c849a
Shouyang Zhou <shouyang@ualberta.ca>
2022-01-14 13:38:28 -0700

    fix: update app shell

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 function App() {
+  return (
+    <div style={{ color: "crimson" }}>
+      <h1>Hello HackED 2022</h1>
+    </div>
+  );
 }
 
 export default App;
